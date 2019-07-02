cfe56bd2cea7ac35eb2f197ec51b4ca07f6360ae
boris7ran <boris.ran7@gmail.com>
2019-07-02 19:06:11 +0200

    Zadatak 2

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -1,12 +1,53 @@
 <template>
     <div>
-        Nesto nesto
+        <table class="table table-striped title-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>BRAND</th>
+                    <th>MODEL</th>
+                    <th>YEAR</th>
+                    <th>MAX SPEED</th>
+                    <th>AUTOMATIC</th>
+                    <th>ENGINE</th>
+                    <th>DOORS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(car, index) in cars" :key="index">
+                    <td>{{ car.id }}</td>
+                    <td>{{ car.brand }}</td>
+                    <td>{{ car.model }}</td>
+                    <td>{{ car.year }}</td>
+                    <td>{{ car.maxSpeed }}</td>
+                    <td>{{ car.isAutomatic }}</td>
+                    <td>{{ car.engine }}</td>
+                    <td>{{ car.numberOfDoors }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
-export default {
+import { carsService } from '@/services/CarsService'
 
+export default {
+    data() {
+        return {
+            cars: []
+        }
+    },
+
+    created() {
+        carsService.getAll()
+            .then(response => {
+                this.cars = response.data;
+                console.log(response)
+            }).catch(error => {
+                alert(error);
+            });
+    }
 }
 </script>
 
